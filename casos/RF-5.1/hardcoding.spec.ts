bf82950f678ef75fd45e1c986faa6c6f1829bddc
Oscar Rompich <oscarligo6@gmail.com>
2026-07-31 17:45:43 -0600

    feat: estructura para pruebas de internacionalización

---
+++ casos/RF-5.1/hardcoding.spec.ts
@@ -0,0 +1,2 @@
+/* Prueba cuando un texto no cambia al cambiar el idioma de la página, 
+es decir, cuando un texto está hardcodeado. */
---
+++ casos/RF-5.1/missing.spec.ts
@@ -0,0 +1,2 @@
+/* Prueba cuando no existe una traducción para un texto en el idioma seleccionado, 
+es decir, cuando le falta la traducción al texto. */
