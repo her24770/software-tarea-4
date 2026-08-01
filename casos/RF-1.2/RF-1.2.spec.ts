500aaacb75a9350e0da86f15c1216758193980f4
stan-2021131 <sergiocoromac@gmail.com>
2026-07-31 18:39:17 -0600

    test: Pruebas para el requisito funcional 1.2

---
+++ casos/RF-1.2/RF-1.2.spec.ts
@@ -0,0 +1,60 @@
+import {test, expect, Page} from '@playwright/test';
+
+const URL = "https://gt.nic.gt/"
+
+async function get_section_noticias(page:Page){
+   return page.locator('section').filter({
+      has: page.getByRole("heading", {name: /Novedades y noticias|News and Updates/i})
+   })
+}
+
+test.describe('RF-1.2 - Sección de noticias', () => {
+   //RF-1.2-01: Existen 3 publicaciones de la sección noticias de news.registro.gt
+   test('TC-01 - has 3 news', async ({page}) => {
+   await page.goto(URL);
+   
+   const section_noticias = await get_section_noticias(page);
+   const articulos = section_noticias.locator('article');
+   await expect(articulos).toHaveCount(3)
+   });
+
+
+   //RF-1.2-02: Las publicaciones se componen de título, fecha y una breve descripción
+   test('TC-02 - Each news has title, date and description', async ({page}) => {
+   await page.goto(URL);
+   
+   const section_noticias = await get_section_noticias(page);
+
+   const articulos = section_noticias.locator('article');
+   const cant_articulos = await articulos.count();
+
+   for (let i = 0; i < cant_articulos; i++) {
+      const articulo = articulos.nth(i);
+      const titulo = articulo.locator('h3');
+      const fecha = articulo.locator('p').first();
+      const descripcion = articulo.locator('p').last();
+      await expect(titulo).toBeVisible()
+      await expect(fecha).toBeVisible()
+      await expect(descripcion).toBeVisible()
+   }
+   });
+
+   //RF-1.2-03: Cada articulo tiene un elemento para acceder a más información
+   test('TC-03 - Each news has a "See more" control', async ({ page }) => {
+    await page.goto(URL);
+
+    const section_noticias = await get_section_noticias(page);
+
+    const articulos = section_noticias.locator('article');
+    const cant_articulos = await articulos.count();
+
+    for (let i = 0; i < cant_articulos; i++) {
+        const control = articulos.nth(i).locator('a, button');
+
+        await expect(control).toHaveCount(1);
+        await expect(control).toBeVisible();
+        await expect(control).toHaveText(/See more|Ver más/i);
+    }
+});
+   
+});
