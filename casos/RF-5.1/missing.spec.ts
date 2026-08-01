e051eb108bf5712ee38af8a4fa1aa547acf0359d
Oscar Rompich <oscarligo6@gmail.com>
2026-07-31 18:10:23 -0600

    feat: prueba de texto faltante

--- casos/RF-5.1/missing.spec.ts
+++ casos/RF-5.1/missing.spec.ts
@@ -1,2 +1,24 @@
 /* Prueba cuando no existe una traducción para un texto en el idioma seleccionado, 
 es decir, cuando le falta la traducción al texto. */
+
+import { test, expect } from '@playwright/test';
+
+test.describe('Prueba de textos faltantes', () => {
+    test('Texto faltante en español', async ({ page }) => {
+        await page.goto('https://dev2.registro.gt/en'); 
+
+        // Verifica que el texto esperado esté presente en la página
+        const textoEsperado = 'Inicio';
+        const elemento = await page.locator(`text=${textoEsperado}`);
+        await expect(elemento).toBeVisible();
+    });
+
+    test('Texto faltante en inglés', async ({ page }) => {
+        await page.goto('https://dev2.registro.gt/en');
+
+        // Verifica que el texto esperado esté presente en la página
+        const textoEsperado = 'Home';
+        const elemento = await page.locator(`text=${textoEsperado}`);
+        await expect(elemento).toBeVisible();
+    });             
+})
